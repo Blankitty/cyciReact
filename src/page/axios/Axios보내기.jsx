2a3d646f2baec2f66cfa6e62481d72e2b0dfa2cb
kjs <kjs74962880@gmail.com>
2025-12-23 11:02:17 +0900

    axios보내기

--- src/page/axios/Axios보내기.jsx
+++ src/page/axios/Axios보내기.jsx
@@ -1,4 +1,5 @@
 import  axios from 'axios';
+import { useState } from 'react';
 
 export default function Study() {
 
@@ -8,11 +9,72 @@ export default function Study() {
             console.log(res);
         });
     }
+
+    function test2() {
+        axios.get('http://localhost:8080/data1', {params:{'text':'hello'}})
+        .then(res => {
+            console.log(res);
+        })
+    }
+    function post1() {
+        axios.get('http://localhost:8080/data2', {params:{name: 'aaa',age: 20}})
+        .then(res => {
+            console.log(res);
+        })
+    }
+    function res1() {
+        axios.get('http://localhost:8080/res1', {params:{name: 'aaa',age: 20}})
+        .then(res => {
+            console.log(res);
+        })
+    }
+    function test4() {
+        axios.get('http://localhost:8080/data3', {params:{'num1': 10, 'num2' : 5}})
+        .then(res => {
+            console.log(res);
+        })
+    }
+
+    // const handleLogin = () => {
+    // const dataDTO = { name, age };
+    // axios.post('http://localhost:8080/login', dataDTO)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => console.error(err));
+    // };
+
+    // const [name, setName] = useState('');
+    // const [age, setAge] = useState('');
+
+
+
+
+
     return (
         <div>
             <h1>Axios 사용해보기</h1>
             <input type="button" value="axios보내기1"
                 onClick={test1} />
+            <input type="button" value="axios보내기2"
+                onClick={test2} />
+
+            <input type="text" value="name" onChange={ e => {
+                e.preventDefault();
+                setName(e.target.value);
+
+            }}/>
+            <input type="text" value="age" onChange={ e => {
+                e.preventDefault();
+                setAge(e.target.value);
+
+            }}/>
+            <input type="button" value="axios보내기3"
+                onClick={post1} />
+            
+            <input type="button" value="axios보내기4"
+                onClick={test4} />
+
+            <input type="button" value="axios보내기5"
+                onClick={res1} />
         </div>
     )
 }
